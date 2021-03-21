c72cd1c22ffcc46f6b643aeada1feaf51b1fa10a
miao704g <g.davide2005@gmail.com>
2021-03-21 13:57:54 +0100

    Initial Commit

---
+++ functions.js
@@ -0,0 +1,115 @@
+document.onkeydown = (e) => {
+    console.log(e.key)
+    switch (e.key) {
+        case '0':
+            document.getElementById('0').play();
+            break;
+        case '1':
+            document.getElementById('1').play();
+            break;
+        case '2':
+            document.getElementById('2').play();
+            break;
+        case '3':
+            document.getElementById('3').play();
+            break;
+        case '4':
+            document.getElementById('4').play();
+            break;
+        case '5':
+            document.getElementById('5').play();
+            break;
+        case '6':
+            document.getElementById('6').play();
+            break;
+        case '7':
+            document.getElementById('7').play();
+            break;
+        case '8':
+            document.getElementById('8').play();
+            break;
+        case '9':
+            document.getElementById('9').play();
+            break;
+        case 'A':
+            document.getElementById('A').play();
+            break;
+        case 'B':
+            document.getElementById('B').play();
+            break;
+        case 'C':
+            document.getElementById('C').play();
+            break;
+        case 'D':
+            document.getElementById('D').play();
+            break;
+        case 'E':
+            document.getElementById('E').play();
+            break;
+        case 'F':
+            document.getElementById('F').play();
+            break;
+        case 'G':
+            document.getElementById('G').play();
+            break;
+        case 'H':
+            document.getElementById('H').play();
+            break;
+        case 'I':
+            document.getElementById('I').play();
+            break;
+        case 'J':
+            document.getElementById('J').play();
+            break;
+        case 'K':
+            document.getElementById('K').play();
+            break;
+        case 'L':
+            document.getElementById('L').play();
+            break;
+        case 'M':
+            document.getElementById('M').play();
+            break;
+        case 'N':
+            document.getElementById('N').play();
+            break;
+        case 'O':
+            document.getElementById('O').play();
+            break;
+        case 'P':
+            document.getElementById('P').play();
+            break;
+        case 'Q':
+            document.getElementById('Q').play();
+            break;
+        case 'R':
+            document.getElementById('R').play();
+            break;
+        case 'S':
+            document.getElementById('S').play();
+            break;
+        case 'T':
+            document.getElementById('T').play();
+            break;
+        case 'U':
+            document.getElementById('U').play();
+            break;
+        case 'V':
+            document.getElementById('V').play();
+            break;
+        case 'W':
+            document.getElementById('W').play();
+            break;
+        case 'X':
+            document.getElementById('X').play();
+            break;
+        case 'Y':
+            document.getElementById('Y').play();
+            break;
+        case 'Z':
+            document.getElementById('Z').play();
+            break;
+        default:
+            console.log("Key is not found!");    
+    }
+};
